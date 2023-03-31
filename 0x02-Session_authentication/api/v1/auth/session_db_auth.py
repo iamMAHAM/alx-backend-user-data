7b15f78f8eced7e3ead8764a6865c08b0b06e48f
iamMAHAM <abdulkabore@gmail.com>
2023-03-31 09:17:22 +0000

    fix some error

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -13,22 +13,25 @@ class SessionDBAuth(SessionExpAuth):
 
     def create_session(self, user_id: str = None) -> str:
         """  returns a Session ID and creates a UserSession record in DB if user_id is valid """
-            
+     
         if user_id is None or isinstance(user_id, str) is False:
             return None
         else:
+
             session_id = SessionExpAuth().create_session(user_id)
+            if (self.user_id_for_session_id(session_id) is not None):
+                return self.user_id_for_session_id(session_id)
             UserSession(user_id=user_id, session_id=session_id).save()
             return session_id
 
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns User ID based on Session ID """
+        """  returns the User ID linked to a UserSession created before with """
 
         if session_id is None or isinstance(session_id, str) is False:
             return None
         else:
-            return
+            return UserSession.search({'session_id': session_id}).first().user_id
 
     def destroy_session(self, request=None):
         """ Deletes user session to logout """
